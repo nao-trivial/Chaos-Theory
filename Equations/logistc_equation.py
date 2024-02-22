292982cd932cee5bf4dfec0701888414b2f772d1
nao-trivial <hassanprimo500@gmail.com>
2024-02-21 22:29:50 -0300

    add logistc equation

---
+++ Equations/logistc_equation.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+
+'''import matplotlib.pyplot as plt
+
+class EquacaoLogisticaDiscreta:
+    def __init__(self, r, x_inicial, iteracoes):
+        self.r = r
+        self.x = x_inicial
+        self.iteracoes = iteracoes
+        self.valores_x = []
+
+    def calcular(self):
+        for i in range(self.iteracoes):
+            self.x = self.r * self.x * (1 - self.x)
+            self.valores_x.append(self.x)
+
+    def plotar(self):
+        plt.plot(self.valores_x)
+        plt.title('Equação Logística Discreta')
+        plt.xlabel('Iteração')
+        plt.ylabel('x')
+        plt.show()
+
+# Cria uma instância da classe EquacaoLogisticaDiscreta
+equacao_logistica = EquacaoLogisticaDiscreta(r=2.6, x_inicial=0.1, iteracoes=100)
+
+# Calcula e plota a sequência de estados
+equacao_logistica.calcular()
+equacao_logistica.plotar()'''
